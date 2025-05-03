978186e6d2f0b61c89b901265d440d2d6ea5611a
Gabriel-AXl <alexgabriel3357@gmail.com>
2025-05-03 18:55:06 -0300

    Adicionando cors e definindo ip dos microserviços

--- gateway/app/web_server.py
+++ gateway/app/web_server.py
@@ -2,17 +2,27 @@ import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__), "../generated"))
 
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 from app.grpc_stub import GRPCStub
 
 
-VM1_IP = "localhost"
-VM2_IP = "localhost"
+VM1_IP = "192.168.100.20" 
+VM2_IP = "192.168.100.20"
 PRODUCT_ADDR = f"{VM1_IP}:50051"
 ORDER_ADDR = f"{VM2_IP}:50052"
 
 app = FastAPI(title="Gateway")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 stub = GRPCStub(
     product_addr=PRODUCT_ADDR,
     order_addr=ORDER_ADDR
